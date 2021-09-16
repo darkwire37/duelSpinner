0ba3145881e2c74fd7e7f9f9c680d54e6ee94938
darkwire37 <71898092+darkwire37@users.noreply.github.com>
2021-09-16 09:20:29 -0400

    Uploaded Source

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1624305382,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/dial-sheet0.png",
+		"images/dial-sheet1.png",
+		"images/dial-sheet2.png",
+		"images/arrow-sheet0.png",
+		"images/token-sheet0.png",
+		"images/token-sheet1.png",
+		"images/settext.png",
+		"images/glow-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
